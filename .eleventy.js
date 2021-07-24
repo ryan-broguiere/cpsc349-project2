3d00c853233af2fd46397aeda5f97ae1a5d283b0
ryan <ryanbroguiere123@gmail.com>
2021-07-24 14:22:17 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/main.css");
     eleventyConfig.addPassthroughCopy("styles/flexbox.css");
     eleventyConfig.addPassthroughCopy("styles/grid.css");
-<<<<<<< HEAD
     eleventyConfig.addPassthroughCopy("styles/tailwind.css");
- };
-=======
-};
->>>>>>> 6e0104cbe9bc8a2d37b4094f2cd6f88e0cb52d92
+ }
+
